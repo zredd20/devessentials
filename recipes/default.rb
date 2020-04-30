2e816f84f7bf291d3f3143cbbfc478c6ef247d7c
Zach Redding <zacharyredding@gmail.com>
2020-04-29 19:21:46 -0500

    adds apache recipe to recipes/default.rp

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+#This is my first attempt at using nano
+#This is my second attempt at using nano
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1> Hello, world! </h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
